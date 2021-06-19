4e8ee3f3466ff75e257f4a40c214865fadcfbe76
Daniel Dai <69875004+danielsdai@users.noreply.github.com>
2021-06-18 20:09:16 -0700

    Full Stack Vue.js, Express & MongoDB [3] - Prepare & Deploy
    Added static folder and prepared SPA

--- server/index.js
+++ server/index.js
@@ -12,6 +12,12 @@ const posts = require("./routes/api/posts");
 
 app.use("/api/posts", posts);
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+    // Static folder
+    app.use(express.static(__dirname + '/public'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
