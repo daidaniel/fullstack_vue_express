2ac11b9cd0ee8e47feae6992ca34bfd0e869d19e
Daniel Dai <69875004+danielsdai@users.noreply.github.com>
2021-06-19 00:18:00 -0700

    Full Stack Vue.js, Express & MongoDB [3] - Prepare & Deploy

--- server/index.js
+++ server/index.js
@@ -16,6 +16,9 @@ app.use("/api/posts", posts);
 if (process.env.NODE_ENV === 'production') {
     // Static folder
     app.use(express.static(__dirname + '/public'));
+
+    // Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 const port = process.env.PORT || 5000;
